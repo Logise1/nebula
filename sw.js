d43ff5f95259df0a0327362a61d02dea0b629d37
Logise1 <logise1123@gmail.com>
2025-11-20 22:46:34 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'nebula-chat-v1';
+const ASSETS_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  // Añade aquí tus iconos si los descargas localmente
+];
+
+// Instalación: Cachear archivos básicos
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activación: Limpiar cachés viejas
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch: Servir desde caché si no hay internet
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
